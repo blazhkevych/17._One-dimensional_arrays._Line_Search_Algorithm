91543a6388614006a9d3c91dba2bf4356cf5f289
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-02-01 18:51:09 +0200

    +1 задание

--- task_1/task_1.cpp
+++ task_1/task_1.cpp
@@ -12,8 +12,64 @@ using std::endl;
 int main()
 {
 	setlocale(LC_ALL, ""); // работает только с потоком вывода
+	srand(time(0));
 
+	int min{ 0 };
+	int max{ 20 };
+	const int arr_1_Size{ 10 };
+	const int arr_2_Size{ 8 };
+	const int arr_3_Size{ 8 };
 
+	int arr_1[arr_1_Size]{};
+	int arr_2[arr_2_Size]{};
+	int arr_3[arr_3_Size]{};
+
+	for (int i = 0; i < arr_1_Size; i++) // заполняем 1 массив и выводим
+	{
+		arr_1[i] = rand() % (max - min + 1) + min;
+		cout << arr_1[i] << ' ';
+	}
+	cout << endl;
+
+	for (int i = 0; i < arr_2_Size; i++) // заполняем 2 массив и выводим
+	{
+		arr_2[i] = rand() % (max - min + 1) + min;
+		cout << arr_2[i] << ' ';
+	}
+	cout << endl;
+
+	for (int i = 0; i < arr_3_Size; i++) // заполняем 3 массив "-1"
+		arr_3[i] = -1;
+
+	for (int i = 0; i < arr_1_Size; i++)
+	{
+		for (int j = 0; j < arr_2_Size; j++)
+		{
+			if (arr_1[i] == arr_2[j])
+			{
+				for (int k = 0; k < arr_3_Size; k++)
+				{
+					if (arr_1[i] == arr_3[k])
+						break;
+					else
+					{
+						if (arr_3[k] == -1)
+						{
+							arr_3[k] = arr_1[i];
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < arr_3_Size; i++) // выводим 3 массив
+	{
+		if (arr_3[i] != -1)
+			cout << arr_3[i] << ' ';
+	}
+	cout << endl;
 
 	return 0;
 }
