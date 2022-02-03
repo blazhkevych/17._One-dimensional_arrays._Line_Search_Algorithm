22efb757d52b075054e05031390b8f9f41497331
Виктор Блажкевич <blazhkevych.va@gmail.com>
2022-02-03 13:27:54 +0200

    +3 задание готово

--- task_3/task_3.cpp
+++ task_3/task_3.cpp
@@ -13,8 +13,59 @@ using std::endl;
 int main()
 {
 	setlocale(LC_ALL, ""); // работает только с потоком вывода
+	srand(time(0));
 
+	int min{ -10 };
+	int max{ 20 };
+	const int arr_1_Size{ 20 };
 
+	int arr_1[arr_1_Size]{ -5,  15, 0,  -15 ,8, -10, 0, -1, 16, 12, 0 , 20, 6, 8, 17, 4, 10, 3,19 ,16 };
 
+	for (int i = 0; i < arr_1_Size; i++) // заполняем 1 массив и выводим
+	{
+		arr_1[i] = rand() % (max - min + 1) + min;
+		cout << arr_1[i] << ' ';
+	}
+	cout << endl;
+
+	int start_position{ 0 }, max_start_position{ 0 },
+		amount_of_numbers{ 0 }, max_amount_of_numbers{ 0 };
+	for (int i = 0; i < arr_1_Size; i++) // 1й массив 
+	{
+		if (arr_1[i] > 0)
+		{
+			amount_of_numbers++;
+			continue;
+		}
+		else
+		{
+			if (amount_of_numbers > max_amount_of_numbers)
+			{
+				max_amount_of_numbers = amount_of_numbers;
+				max_start_position = start_position;
+			}
+		}
+		amount_of_numbers = 0;
+		start_position = i;
+	}
+
+	if (amount_of_numbers > max_amount_of_numbers) // для цепочки в конце
+	{
+		max_amount_of_numbers = amount_of_numbers;
+		max_start_position = start_position;
+	}
+
+	cout << "Максимальная длина цепочки: " << max_amount_of_numbers << endl;
+
+	if (max_start_position == 0) // распределение вывода на вывод для "цепи в начале" и вывод для "цепи в средине и конце"
+	{
+		for (int i = max_start_position; i < max_start_position + max_amount_of_numbers; i++) // выводим фрагмент для цепи в начале
+			cout << arr_1[i] << ' ';
+	}
+	else
+	{
+		for (int i = max_start_position + 1; i < max_start_position + max_amount_of_numbers + 1; i++) // выводим фрагмент для цепи в средине и конце
+			cout << arr_1[i] << ' ';
+	}
 	return 0;
 }
--- task_4/task_4.cpp
+++ task_4/task_4.cpp
@@ -11,8 +11,31 @@ using std::endl;
 int main()
 {
 	setlocale(LC_ALL, ""); // работает только с потоком вывода
+	srand(time(0));
 
+	int min{ -20 };
+	int max{ 20 };
+	const int arr_1_Size{ 10 };
 
+	int arr_1[arr_1_Size]{ 0 };
+
+	for (int i = 0; i < arr_1_Size; i++) // заполняем 1 массив и выводим
+	{
+		arr_1[i] = rand() % (max - min + 1) + min;
+		cout << arr_1[i] << ' ';
+	}
+	cout << endl;
+
+	for (int i = 0; i < arr_1_Size; i++)
+	{
+		if (arr_1[i] >= 0) // Для положительного числа и нуля модуль равен самомý числу
+			arr_1[i] = arr_1[i];
+		else if (arr_1[i] < 0) // а для отрицательного – противоположному числу;
+			arr_1[i] = arr_1[i]*-1;
+	}
+
+	for (int i = 0; i < arr_1_Size; i++) // выводим 1 массив с модулями
+		cout << arr_1[i] << ' ';
 
 	return 0;
 }
